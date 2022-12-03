0e0d7123c9fe291381dbd2d6a219df117f11665c
Cabrel Ken <cabrelk@gmail.com>
2022-12-03 12:39:13 +0100

    creation de la vue du formulaire

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Progressor ( compo API)" />
+    <Form />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Form from "@/components/Form.vue";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+    Form,
   },
 };
 </script>
